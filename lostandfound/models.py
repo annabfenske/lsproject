2a9e50bf88e6c3f307015cc8eca6834164e7262c
cp2379 <cp2379@nyu.edu>
2017-11-29 15:55:15 -0500

    merging

--- lostandfound/models.py
+++ lostandfound/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    net_id = models.CharField("Net ID", max_length=10, blank=False, null=False, primary_key=True)
+    n_number = models.CharField("N#", max_length=10, blank=False, null=False)
+    first_name = models.CharField("First Name", max_length=250, blank=False, null=False)
+    last_name = models.CharField("Last Name", max_length=250, blank=False, null=False)
+    nyu_email = models.EmailField(max_length=254)
+    id_lost = models.BooleanField(blank=False, null=False, default=False)
