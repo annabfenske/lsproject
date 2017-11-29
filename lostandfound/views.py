c9d0dec66c24aec2920cd08195d8a535e71e326f
Aditya Sawhney <aksawhney17@gmail.com>
2017-11-29 16:49:57 -0500

    Edit views

--- lostandfound/views.py
+++ lostandfound/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.core.cache import cache
+import time
 
 # Create your views here.
 
 def swipe(request, netID):
-    # Check if id is lost
-    # cache.get(netID)?
-    # cache.write(swipe)
-    return JsonResponse({'lost':False})
+    expiryTime = 36000 # 10 hours
+    currentTime = time.time()
+
+    swipe = {
+      'netID': netID,
+      'time': currentTime
+    }
+    cache.write(swipe)
+    reportTime = cache.get(netID)
+    
+    if !reportTime:
+      return JsonResponse({'lost':False})
+
+    if reportTime < currentTime - expiryTime:
+      return JsonResponse({'lost':False})
+    
+    return JsonResponse({'lost':True})
 
 def readUser(request, netID):
     # Should we check postGres or just redis? Recency of swipes?
