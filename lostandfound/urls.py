9e95bc9c214f4d27ce3a3c52b345267f229ea2ec
Aditya Sawhney <aksawhney17@gmail.com>
2017-11-29 16:26:02 -0500

    urls

--- lostandfound/urls.py
+++ lostandfound/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', view.index, name='index')
+    url(r'^$', view.index, name='index'),
+    url(r'^swipes/(?P<netID>[a-zA-Z0-9]+/$)', views.swipe),
+    url(r'^users/(?P<netID>[a-zA-Z0-9]+/$)', views.readUser),
+    url(r'^users/$', views.createUser)
 ]
--- lostandfound/views.py
+++ lostandfound/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def swipe(request, netID):
+    # Check if id is lost
+    # cache.get(netID)?
+    # cache.write(swipe)
+    return JsonResponse({'lost':False})
+
+def readUser(request, netID):
+    # Should we check postGres or just redis? Recency of swipes?
+    return render(request, 'lostandfound/swipehistory.html')
+
+# def createUser(request, netID):
