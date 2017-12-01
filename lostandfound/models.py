a05856642694c320361f3e34d777649838f709c1
cp2379 <cp2379@nyu.edu>
2017-11-30 21:36:03 -0500

    user model working!

--- lostandfound/models.py
+++ lostandfound/models.py
@@ -3,4 +3,4 @@ from django.forms import ModelForm
 
 class User(models.Model):
     net_id = models.CharField("Net ID", max_length=10, blank=False, primary_key=True)
-    time_lost = models.DateTimeField()
+    time_lost = models.DateTimeField(auto_now_add=True, blank=False, null=True)
--- lostandfound/views.py
+++ lostandfound/views.py
@@ -3,10 +3,13 @@ from django.http import HttpResponse
 from .models import User
 from .forms import UserForm
 from django.template import RequestContext
+from datetime import datetime
 
 def reportLost(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
+        print('form before:', form)
+        print('form after:', form)
         if form.is_valid():
             form.save()
         print('here - 1')
