4d7cd7a451dcf48236d09b651c3073123e097da8
cp2379 <cp2379@nyu.edu>
2017-12-01 14:49:28 -0500

    fixed post view, error handling working, cache attempt 1

--- lostandfound/urls.py
+++ lostandfound/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-#/reportLost/
-    #url(r'^$', views.reportLost, name='reportLost'),
+    #/reportLost/
     url(r'^$', views.reportLost, name='reportLost'),
 ]
--- lostandfound/views.py
+++ lostandfound/views.py
@@ -1,23 +1,20 @@
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render
 from django.http import HttpResponse
 from .models import User
 from .forms import UserForm
-from django.template import RequestContext
 from datetime import datetime
+from django.core.cache import cache
+from django.views.decorators.cache import cache_page
 
+#@cache_page(60 * 15)
 def reportLost(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
-        print('form before:', form)
-        print('form after:', form)
         if form.is_valid():
             form.save()
-        print('here - 1')
-        return render(request,'reportLost/index.html', {form : form})
-    else:
-        form = UserForm()
+            #cache.write(form)
+            cache.set(form,datetime.now)
+    form = UserForm()
     args = {}
-
     args['form'] = form
-    print('here - 2')
     return render(request,'reportLost/index.html', args)
