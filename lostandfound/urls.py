a44d80f82d3b6a3a446d1be9513ef5b41b8270a1
Aditya Sawhney <aksawhney17@gmail.com>
2017-12-01 21:52:08 -0500

    swipes and swipe history work but missing location rn

--- lostandfound/urls.py
+++ lostandfound/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^swipes/(?P<netID>[a-zA-Z0-9]+/$)', views.swipe, name='swipe'),
-    url(r'^users/(?P<netID>[a-zA-Z0-9]+/$)', views.readUser, name='readUser'),
+    url(r'^swipes/(?P<netID>[a-zA-Z0-9]+)/$', views.swipe, name='swipe'),
+    url(r'^users/(?P<netID>[a-zA-Z0-9]+)/$', views.readUser, name='readUser'),
     url(r'^users/$', views.createUser, name='createUser')
 ]
--- lostandfound/views.py
+++ lostandfound/views.py
@@ -1,26 +1,35 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.core.cache import cache
+from django_redis import get_redis_connection
+
 import time
 import uuid
+import json
 
-# Create your views here.
+
+redis = get_redis_connection('default')
 
 def index(request):
-  return JsonResponse({"placeholder":True})
+  return JsonResponse({'placeholder':True})
 
 def swipe(request, netID):
     expiryTime = 36000 # 10 hours
     currentTime = time.time()
 
-    tempID = uuid.uuid4()
     swipe = {
       'netID': netID,
       'time': currentTime
     }
-    cache.set(tempID, swipe)
-    reportTime = cache.get(netID)
-    
+
+    # if request.body.location:
+    #   swipe['location'] = request.body.location
+    swipe['location'] = 'Bobst'
+
+    redis.rpush(':1:swipes_' + netID, json.dumps(swipe))
+
+    reportTime = cache.get('lost_' + netID)
+
     if not reportTime:
       return JsonResponse({'lost':False})
 
@@ -30,8 +39,13 @@ def swipe(request, netID):
     return JsonResponse({'lost':True})
 
 def readUser(request, netID):
-    # Should we check postGres or just redis? Recency of swipes?
-    return render(request, 'lostandfound/swipehistory.html')
+    swipes = redis.lrange(':1:swipes_' + netID, -20, -1)
+    swipes = map(json.loads, swipes)
+    print swipes
+    context = {'headers': ['Time', 'Location'], 
+              'keys': ['time', 'location'],
+              'swipes': swipes}
+    return render(request, 'swipe_history.html', context)
 
 def reportLost(request, netID):
   return JsonResponse({"placeholder":True})
