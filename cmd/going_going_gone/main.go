71b33e1d92bab8b0c998bdeebb39a33762fb0e7b
jeremymuhia <jmuhia80@gmail.com>
2017-12-01 20:36:52 -0500

    testing redis connection

--- cmd/going_going_gone/main.go
+++ cmd/going_going_gone/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
     "fmt"
+    "os"
+    redis "github.com/garyburd/redigo/redis"
 )
 
 func main() {
-    fmt.Println("Hello, world 69")
+    cursor, err := redis.DialURL(os.Getenv("REDIS_URL"))
+    if err != nil {
+        fmt.Println("$$$ could not connect to redis")
+    }
+    defer cursor.Close()
+    fmt.Println("$$$ successfully connected to redis: %s", os.Getenv("REDIS_URL"))
+    os.Exit(0)
 }
