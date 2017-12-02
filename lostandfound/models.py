fcce11ad384423120ec408b266e9a2277262c32e
cp2379 <cp2379@nyu.edu>
2017-12-01 22:30:44 -0500

    updated model to index by time

--- lostandfound/models.py
+++ lostandfound/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.conf import settings
 
-class User(models.Model):
-    net_id = models.CharField("Net ID", max_length=10, blank=False, primary_key=True)
-    time_lost = models.DateTimeField(auto_now_add=True, blank=False)
+class lostNYUID(models.Model):
+    net_id = models.CharField("Net ID", max_length=10, blank=False)
+    time_lost = models.DateTimeField(primary_key=True, blank=False)
 
     def __str__(self):
         return 'time lost' + str(self.time_lost)
--- lostandfound/views.py
+++ lostandfound/views.py
@@ -1,19 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import User
+from .models import lostNYUID
 from .forms import UserForm
 from django.core.cache import cache
 from datetime import datetime
 
 def reportLost(request):
-    print(User.objects.all())
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
             ID = form.save(commit=False)
             ID.time_lost = datetime.now()
             ID.save()
-            cache.set(form.cleaned_data.get('net_id'), ID.time_lost)
     form = UserForm()
     args = {}
     args['form'] = form
